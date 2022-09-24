23c132d7facd3b0c020ef2fd5034ce72042a2170
yrom1 <ryanm.inbox@gmail.com>
2022-09-24 15:50:05 -0400

    Use context manager for StarSchema so it commits

--- cloud.py
+++ cloud.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     from main import COUNTS, DAYS
 
     ISSUES_DONE_TODAY: int = int(COUNTS[-1])
-    StarSchema().insert_dimension("dimension_jira", [ISSUES_DONE_TODAY])
+    with StarSchema() as db:
+        db.insert_dimension("dimension_jira", [ISSUES_DONE_TODAY])
     query = """
     SELECT SUM(j.issues_done)
     FROM fact_table f
@@ -19,7 +20,8 @@ if __name__ == "__main__":
             ON f.id_jira = j.id
     WHERE month(f.date) = month(now());
     """
-    ISSUES_DONE_THIS_MONTH: int = StarSchema().query(query)[0][0]
+    with StarSchema() as db:
+        ISSUES_DONE_THIS_MONTH: int = db.query(query)[0][0]
     print(ISSUES_DONE_TODAY, ISSUES_DONE_THIS_MONTH)
 
     # NOTE uncomment this code when you want it to be live on the site
