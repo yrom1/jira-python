73949c5a6872780cd1f180ac2fb42fd5f8f90fb2
yrom1 <ryanm.inbox@gmail.com>
2022-09-24 17:13:42 -0400

    Add mypandas and df queries for transition period

--- cloud.py
+++ cloud.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
     from main import COUNTS, DAYS
 
     ISSUES_DONE_TODAY: int = int(COUNTS[-1])
+    Cloud("kpiV1")["ISSUES_DONE_TODAY"] = ISSUES_DONE_TODAY
     with StarSchema() as db:
         db.insert_dimension("dimension_jira", [ISSUES_DONE_TODAY])
+
     query = """
     SELECT SUM(j.issues_done)
     FROM fact_table f
@@ -23,11 +25,21 @@ if __name__ == "__main__":
     with StarSchema() as db:
         ISSUES_DONE_THIS_MONTH: int = db.query(query)[0][0]
     print(ISSUES_DONE_TODAY, ISSUES_DONE_THIS_MONTH)
-
-    # NOTE uncomment this code when you want it to be live on the site
+    # NOTE new RDS system, wait for new month to use
     # Cloud("kpiV1")["ISSUES_DONE_THIS_MONTH"] = ISSUES_DONE_THIS_MONTH
-    # Cloud("kpiV1")["ISSUES_DONE_TODAY"] = ISSUES_DONE_TODAY
-    # df = pd.DataFrame({"date": DAYS, "value": COUNTS})
-    # df.to_json("plot.json")
-    # with open("plot.json", "r") as f:
-    #     Cloud("plotsV2")["jira"] = f.read()
+    # NOTE old mypandas system
+    df = pd.DataFrame({"date": DAYS, "value": COUNTS})
+    Cloud("kpiV1")["ISSUES_DONE_THIS_MONTH"] = int(
+        MyPandas("mysql://root:root@localhost")(
+            """
+    SELECT SUM(value)
+    FROM df
+    WHERE month(date) = month(now());
+    """,
+            locals(),
+        ).values[0]
+    )
+
+    df.to_json("plot.json")
+    with open("plot.json", "r") as f:
+        Cloud("plotsV2")["jira"] = f.read()
